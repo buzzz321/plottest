779c5e513be8ed545db8437287f37a75972a5d67
Anders Olme <anders.olme@gmail.com>
2023-05-18 09:59:02 +0200

    First checkin
    TRy to play with raylib from zig.
    Looks smooth just a shame that we dont have better opengl in mac os

---
+++ build.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "sdltest",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    exe.linkSystemLibrary("raylib");
+    exe.linkSystemLibrary("c");
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const c = @cImport({
+    @cInclude("raylib.h");
+});
+
+const WINDOW_WIDTH: i32 = 1024;
+const WINDOW_HEIGHT: i32 = 700;
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    c.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib [core] example - keyboard input");
+    defer c.CloseWindow();
+
+    c.SetTargetFPS(60);
+
+    while (!c.WindowShouldClose()) {
+        c.BeginDrawing();
+        {
+            c.ClearBackground(c.RAYWHITE);
+            c.DrawText("move the ball with arrow keys", 10, 10, 20, c.DARKGRAY);
+        }
+        c.EndDrawing();
+    }
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+}
