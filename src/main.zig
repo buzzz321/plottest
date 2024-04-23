44846b2fa730483b406203e442bb89c5ff064370
Anders Olme <anders.olme@gmail.com>
2024-04-23 14:23:08 +0200

    Update to latest zig
    Now we support 0.12 version

--- src/main.zig
+++ src/main.zig
@@ -11,11 +11,11 @@ const WINDOW_HEIGHT: i32 = 700;
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
+    const fps: i32 = 60;
     c.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib [core] example - keyboard input");
     defer c.CloseWindow();
 
-    c.SetTargetFPS(60);
+    c.SetTargetFPS(fps);
 
     while (!c.WindowShouldClose()) {
         c.BeginDrawing();
